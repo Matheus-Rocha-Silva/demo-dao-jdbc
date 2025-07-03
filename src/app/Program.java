fb59a8ccc211b856a0c22f6fdbbebfd4334eeea9
Matheus-Rocha-Silva <matheus2244@gmail.com>
2025-07-03 14:15:25 -0300

    seller entity class

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,11 +1,15 @@
 package app;
 
+
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
 		Department obj = new Department(1, "Books");
-		
-		System.out.println(obj);
+		Seller seller = new Seller();
+		System.out.println(seller);
 	}
 }
